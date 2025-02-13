f24ceb87d0b8f846de163f8b6eb3e1bcd93dc4b1
Honah J <honahx@apache.org>
2025-02-12 16:46:58 -0800

    commit mysterious re-format of PolicyEntity, may need to revert later

--- polaris-core/src/main/java/org/apache/polaris/core/entity/PolicyEntity.java
+++ polaris-core/src/main/java/org/apache/polaris/core/entity/PolicyEntity.java
@@ -16,91 +16,94 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.polaris.core.entity;
 
 import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.rest.RESTUtil;
 
-public class PolicyEntity extends PolarisEntity{
+public class PolicyEntity extends PolarisEntity {
 
-    public static final String POLICY_TYPE_KEY = "policy-type";
-    public static final String POLICY_DESCRIPTION_KEY = "policy-description";
-    public static final String POLICY_VERSION_KEY = "policy-version";
-    public static final String POLICY_CONTENT_KEY = "policy-content";
+  public static final String POLICY_TYPE_KEY = "policy-type";
+  public static final String POLICY_DESCRIPTION_KEY = "policy-description";
+  public static final String POLICY_VERSION_KEY = "policy-version";
+  public static final String POLICY_CONTENT_KEY = "policy-content";
 
-    public PolicyEntity(PolarisBaseEntity sourceEntity) {
-        super(sourceEntity);
+  public PolicyEntity(PolarisBaseEntity sourceEntity) {
+    super(sourceEntity);
+  }
+
+  public static PolicyEntity of(PolarisBaseEntity sourceEntity) {
+    if (sourceEntity != null) {
+      return new PolicyEntity(sourceEntity);
     }
 
-    public static PolicyEntity of(PolarisBaseEntity sourceEntity) {
-        if (sourceEntity != null) {
-            return new PolicyEntity(sourceEntity);
-        }
+    return null;
+  }
 
-        return null;
+  public String getPolicyType() {
+    return getPropertiesAsMap().get(POLICY_TYPE_KEY);
+  }
+
+  public String getDescription() {
+    return getPropertiesAsMap().get(POLICY_DESCRIPTION_KEY);
+  }
+
+  public String getContent() {
+    return getPropertiesAsMap().get(POLICY_CONTENT_KEY);
+  }
+
+  public String getPolicyVersion() {
+    return getPropertiesAsMap().get(POLICY_VERSION_KEY);
+  }
+
+  public static class Builder extends PolarisEntity.BaseBuilder<PolicyEntity, Builder> {
+    public Builder(Namespace namespace, String policyName, String policyType) {
+      super();
+      setType(PolarisEntityType.POLICY);
+      setParentNamespace(namespace);
+      setName(policyName);
+      setPolicyType(policyType);
+      // TODO: check whether version starts from 0
+      setVersion(0);
     }
 
-    public String getPolicyType() {
-        return getPropertiesAsMap().get(POLICY_TYPE_KEY);
+    public Builder(PolicyEntity original) {
+      super(original);
     }
 
-    public String getDescription() {
-        return getPropertiesAsMap().get(POLICY_DESCRIPTION_KEY);
+    @Override
+    public PolicyEntity build() {
+      return new PolicyEntity(buildBase());
     }
 
-    public String getContent() {
-        return getPropertiesAsMap().get(POLICY_CONTENT_KEY);
+    public Builder setParentNamespace(Namespace namespace) {
+      if (namespace != null && !namespace.isEmpty()) {
+        internalProperties.put(
+            NamespaceEntity.PARENT_NAMESPACE_KEY, RESTUtil.encodeNamespace(namespace));
+      }
+      return this;
     }
 
-    public String getPolicyVersion() {
-        return getPropertiesAsMap().get(POLICY_VERSION_KEY);
+    public Builder setPolicyType(String policyType) {
+      // TODO: Do we need to validate the type here?
+      properties.put(POLICY_TYPE_KEY, policyType);
+      return this;
     }
 
-    public static class Builder extends PolarisEntity.BaseBuilder<PolicyEntity, Builder> {
-        public Builder(Namespace namespace, String policyName, String policyType) {
-            super();
-            setType(PolarisEntityType.POLICY);
-            setParentNamespace(namespace);
-            setName(policyName);
-            setPolicyType(policyType);
-            // TODO: check whether version starts from 0
-            setVersion(0);
-        }
-
-        public Builder(PolicyEntity original) {super(original);}
-
-        @Override
-        public PolicyEntity build() {return new PolicyEntity(buildBase());}
-
-        public Builder setParentNamespace(Namespace namespace) {
-            if (namespace != null && !namespace.isEmpty()) {
-                internalProperties.put(
-                        NamespaceEntity.PARENT_NAMESPACE_KEY, RESTUtil.encodeNamespace(namespace));
-            }
-            return this;
-        }
-
-        public Builder setPolicyType(String policyType) {
-            // TODO: Do we need to validate the type here?
-            properties.put(POLICY_TYPE_KEY, policyType);
-            return this;
-        }
-
-        public Builder setDescription(String description) {
-            properties.put(POLICY_DESCRIPTION_KEY, description);
-            return this;
-        }
-
-        public Builder setVersion(long version) {
-            properties.put(POLICY_VERSION_KEY, Long.toString(version));
-            return this;
-        }
-
-        public Builder setContent(String content) {
-            // TODO: validate, but not here
-            properties.put(POLICY_CONTENT_KEY, content);
-            return this;
-        }
+    public Builder setDescription(String description) {
+      properties.put(POLICY_DESCRIPTION_KEY, description);
+      return this;
     }
+
+    public Builder setVersion(long version) {
+      properties.put(POLICY_VERSION_KEY, Long.toString(version));
+      return this;
+    }
+
+    public Builder setContent(String content) {
+      // TODO: validate, but not here
+      properties.put(POLICY_CONTENT_KEY, content);
+      return this;
+    }
+  }
 }
